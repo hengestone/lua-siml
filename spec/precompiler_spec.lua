93ffcabdb93c806116f5f4007d986796848540dd
Conrad Steenberg <conrad@dstld.la>
2016-08-27 22:16:44 -0700

    Update spec tests

--- spec/precompiler_spec.lua
+++ spec/precompiler_spec.lua
@@ -5,64 +5,63 @@ describe("The LuaSiml Precompiler:", function()
 
   local engine = siml.new()
 
-
   describe("conditional blocks", function()
     it("should handle single if", function()
-      assert_equal("<p>a</p>", engine:render("- if true then\n  p a"))
+      assert.are.equal("<p>a</p>", engine:render("- if true then\n  p a"):gsub("%s", ""))
     end)
 
     it("should handle if/else", function()
-      assert_equal("<p>a</p>", engine:render("- if true then\n  p a\n- else\n  p b"))
+      assert.are.equal("<p>a</p>", engine:render("- if true then\n  p a\n- else\n  p b"):gsub("%s", ""))
     end)
 
     it("should handle if/elseif", function()
-      assert_equal("<p>a</p>", engine:render("- if true then\n  p a\n- elseif false then\n  p b"))
+      assert.are.equal("<p>a</p>", engine:render("- if true then\n  p a\n- elseif false then\n  p b"):gsub("%s", ""))
     end)
 
     it("should handle if/elseif/else", function()
-      assert_equal("<p>a</p>", engine:render("- if true then\n  p a\n- elseif false then\n  p b\n- else\n  p c"))
+      assert.are.equal("<p>a</p>", engine:render("- if true then\n  p a\n- elseif false then\n  p b\n- else\n  p c"):gsub("%s", ""))
     end)
   end)
 
   describe("the endstack", function()
 
     local es
-    before(function()
+    before_each(function()
       es = haml.end_stack.new()
     end)
 
     it("should have an initial indent level of 0", function()
-      assert_equal(es:indent_level(), 0)
+      assert.are.equal(es:indent_level(), 0)
     end)
 
     it("should add an HTML tag and increase the indent level", function()
       es:push("</p>")
-      assert_equal(es:indent_level(), 1)
+      assert.are.equal(es:indent_level(), 1)
     end)
 
     it("should not increase the indent level for code endings", function()
       es:push("end")
-      assert_equal(es:indent_level(), 0)
+      assert.are.equal(es:indent_level(), 0)
     end)
 
     it("should pop an HTML tag an decrease the indent level", function()
       es:push("</html>")
       es:push("</body>")
-      assert_equal(es:indent_level(), 2)
+      assert.are.equal(es:indent_level(), 2)
       es:pop()
-      assert_equal(es:indent_level(), 1)
+      assert.are.equal(es:indent_level(), 1)
     end)
 
     it("should not decrease indent level when popping code endings", function()
       es:push("</html>")
       es:push("end")
-      assert_equal(es:indent_level(), 1)
+      assert.are.equal(es:indent_level(), 1)
       es:pop()
-      assert_equal(es:indent_level(), 1)
+      assert.are.equal(es:indent_level(), 1)
     end)
 
     it("should return nil when popping an empty stack", function()
-      assert_equal(es:pop(), nil)
+      assert.are.equal(es:pop(), nil)
     end)
 
   end)
--- spec/renderer_spec.lua
+++ spec/renderer_spec.lua
@@ -5,18 +5,11 @@ require "siml"
 
 local tests = {
   -- Script
-  {'p="hello"', '<p>hello</p>'},
-  {"- a = 'b'\np=a", "<p>b</p>"},
-  {"- for k,v in pairs({a = 'a'}) do\n  p(class=k)=v", "<p class='a'>a</p>"},
-
-  -- External filters
-  {":markdown\n  # a", "<h1>a</h1>"},
-
-  -- Multiline blocks
-  {"p= 1 + |\n  2 |", "<p>3</p>"},
+  {'p="hello"', '<p>\n  hello\n</p>'},
+  {"- a = 'b'\np=a", "<p>\n  b\n</p>"},
+  {"- for k,v in pairs({a = 'a'}) do\n  p(class=k)=v", "<p class='a'>\n  a\n</p>"},
 
 }
-
 
 describe("The LuaSiml Renderer", function()
   local locals = {}
@@ -24,7 +17,7 @@ describe("The LuaSiml Renderer", function()
     test(string.format("should render '%s' as '%s'", string.gsub(t[1], "\n", "\\n"),
         string.gsub(t[2], "\n", "\\n")), function()
         local engine = siml.new()
-        assert_equal(t[2], engine:render(t[1], locals))
+        assert.are.equal(t[2], engine:render(t[1], locals))
     end)
   end
 
@@ -37,56 +30,56 @@ describe("The LuaSiml Renderer", function()
     local code = "p(id=get_id)"
     local html = "<p id='hello'></p>"
     local engine = siml.new()
-    assert_equal(html, engine:render(code, locals))
+    assert.are.equal(html, engine:render(code, locals))
   end)
 
   test("should suppress_eval with script operators", function()
     local code = "p\n  = 'hello'"
     local html = "<p>\n\n</p>"
     local engine = siml.new({suppress_eval = true})
-    assert_equal(html, engine:render(code, locals))
+    assert.are.equal(html, engine:render(code, locals))
   end)
 
   test("should suppress_eval with tag script operators", function()
     local code = "p= 'hello'"
-    local html = "<p></p>"
+    local html = "<p>\n\n</p>"
     local engine = siml.new({suppress_eval = true})
-    assert_equal(html, engine:render(code, locals))
+    assert.are.equal(html, engine:render(code, locals))
   end)
 
   test("should not interpolate when suppress_eval is set", function()
     local code = "p #{var}"
-    local html = "<p>#{var}</p>"
+    local html = "<p>\n  #{var}\n</p>"
     local engine = siml.new({suppress_eval = true})
-    assert_equal(html, engine:render(code, {var = "hello"}))
+    assert.are.equal(html, engine:render(code, {var = "hello"}))
   end)
 
   test("should not interpolate locals in attributes", function()
     local code = "- local foo = \"bar\"\np(class=\"foo-#{foo}\")"
     local html = "<p class='foo-#{foo}'></p>"
     local engine = siml.new()
-    assert_equal(html, engine:render(code))
+    assert.are.equal(html, engine:render(code))
   end)
 
   test("should not interpolate locals in script", function()
     local code = "- local foo = 'bar'\na= \"foo-#{foo}\""
-    local html = "<a>foo-#{foo}</a>"
+    local html = "<a>\n  foo-#{foo}\n</a>"
     local engine = siml.new()
-    assert_equal(html, engine:render(code))
+    assert.are.equal(html, engine:render(code))
   end)
 
   test("should interpolate globals in attributes", function()
     local code = "- foo = \"bar\"\np(class=\"foo-#{foo}\")"
     local html = "<p class='foo-bar'></p>"
     local engine = siml.new()
-    assert_equal(html, engine:render(code))
+    assert.are.equal(html, engine:render(code))
   end)
 
-  test("should interpolate globals in script", function()
+  test("should not interpolate ruby style code", function()
     local code = "- foo = 'bar'\na= \"foo-#{foo}\""
-    local html = "<a>foo-bar</a>"
+    local html = "<a>\n  foo-#{foo}\n</a>"
     local engine = siml.new()
-    assert_equal(html, engine:render(code))
+    assert.are.equal(html, engine:render(code))
   end)
 
   test("should not call function attributes when suppress_eval is set", function()
@@ -98,6 +91,6 @@ describe("The LuaSiml Renderer", function()
     local code = "p(id=get_id)"
     local html = "<p></p>"
     local engine = siml.new({suppress_eval = true})
-    assert_equal(html, engine:render(code, locals))
+    assert.are.equal(html, engine:render(code, locals))
   end)
 end)
